2e305c3a071c464e0c398f3fb49ebd54409406d8
Grzegorz <grzegorztruty@gmail.com>
2022-06-12 12:38:52 +0200

    Initial commit

---
+++ src/main/java/pl/edu/wszib/projekt_zaliczeniowy/ProjektZaliczeniowyApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.projekt_zaliczeniowy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektZaliczeniowyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjektZaliczeniowyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/edu/wszib/projekt_zaliczeniowy/ProjektZaliczeniowyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.projekt_zaliczeniowy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektZaliczeniowyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
